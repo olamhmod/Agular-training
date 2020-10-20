9a6e8b7a509ad63d63a4f365277d8800541c0728
olamhmod <73137947+olamhmod@users.noreply.github.com>
2020-10-20 15:41:54 +0200

    save changes

--- first-Angular-Experience/src/app/app.module.ts
+++ first-Angular-Experience/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
+
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule.withServerTransition({ appId: 'first-Angular-Experience' }),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
